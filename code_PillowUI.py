ae2d74f78ca4b41fefe8c70dbf02a2f77b911df4
Kirill <Kirill.Teplekov@yandex.ru>
2018-12-19 22:37:05 +0300

    Создал возможность показывать цветовые каналы изображения

--- code_PillowUI.py
+++ code_PillowUI.py
@@ -109,6 +109,38 @@ class App(QMainWindow):
         self.rotation_btn.clicked.connect(self.rotation)
         self.flip_horizontally_btn.clicked.connect(self.flip_horizontally)
         self.flip_vertical_btn.clicked.connect(self.flip_vertical)
+        self.show_channel_btn.clicked.connect(self.show_channel)
+
+    #Показать один из цветовых каналов
+    def show_channel(self):
+        # Создает копию текущего изображения для отображения каналов
+        channel_image = self.load_image
+        pixel = channel_image.load()
+        if self.radioButtin_all.isChecked():
+            channel_image.show()
+        # Показывает красный канал
+        elif self.radioButton_red.isChecked():
+            for i in range(self.width):
+                for j in range(self.height):
+                    r, g, b = pixel[i, j]
+                    pixel[i, j] = r, 0, 0
+            channel_image.show()
+
+        # Показывает синий канал
+        elif self.radioButton_blue.isChecked():
+            for i in range(self.width):
+                for j in range(self.height):
+                    r, g, b = pixel[i, j]
+                    pixel[i, j] = 0, 0, b
+            channel_image.show()
+
+        # Показывает зеленый канал
+        elif self.self.radioButton_green.isChecked():
+            for i in range(self.width):
+                for j in range(self.height):
+                    r, g, b = pixel[i, j]
+                    pixel[i, j] = 0, g, 0
+            channel_image.show()
 
     # Слияние изображений
     def merge_image(self):
